89c02408e80d8de2b62f4455371416e70d8993c0
shaurya002 <shauryapratapsingh112@gmail.com>
2026-08-29 22:03:48 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/shaurya/sharedexpensesplittingapplication/SharedExpenseSplittingApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.sharedexpensesplittingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedExpenseSplittingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedExpenseSplittingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaurya/sharedexpensesplittingapplication/SharedExpenseSplittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.sharedexpensesplittingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedExpenseSplittingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
